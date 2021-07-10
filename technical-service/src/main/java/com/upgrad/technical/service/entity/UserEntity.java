c02dcf04aebffac997100017bfe1e5aa2f8133be
ashutosh <ashu.p89@gmail.com>
2021-07-10 18:57:33 +0530

    entity completed

--- technical-service/src/main/java/com/upgrad/technical/service/entity/UserEntity.java
+++ technical-service/src/main/java/com/upgrad/technical/service/entity/UserEntity.java
@@ -33,12 +33,14 @@ LAST_LOGIN_AT        TIMESTAMP (Null to make its default value as null)
 
 //Write the annotation which defines that a class can be mapped to a Table
 //
+@Entity
 @Table(name = "USERS", schema = "imagehoster")
 public class UserEntity implements Serializable {
 
     //Write the annotation which specifies that id attribute is a primary key
-
-    //The @Column annotation helps to specify the properties of the column to which this field or attribute will be matched.
+    @Id
+    //The @Column annotation helps to specify the properties of the column
+    // to which this field or attribute will be matched.
     //This annotation has 4 attributes name, length, nullable and unique
     //In name attribute you can explicitly specify the name of the column
     //Here the name "ID" is explicitly specified
@@ -52,6 +54,8 @@ public class UserEntity implements Serializable {
     private String uuid;
 
     //Write an annotation with its corresponding attribute to explicitly specify the column name as "ROLE"
+    @Column(name = "ROLE")
+    @Size(max = 36)
     private String role;
 
     @Column(name = "EMAIL")
@@ -60,6 +64,10 @@ public class UserEntity implements Serializable {
     private String email;
 
     //Define a private attribute named "password" of type String and explicitly specify its column name as "PASSWORD"
+    @Column(name = "PASSWORD")
+    @NotNull
+    @Size(max=200)
+    private String password;
 
 
     @Column(name = "FIRST_NAME")
@@ -69,7 +77,14 @@ public class UserEntity implements Serializable {
     private String firstName;
 
 
-    //Define a private attibute named "lastName" of type String, explictly specify its column name as "LAST_NAME" with notnull constraint and its maximum size should be 200
+    //Define a private attibute named "lastName" of type String,
+    // explictly specify its column name as "LAST_NAME"
+    // with notnull constraint and its maximum size should be 200
+
+    @Column(name = "LAST_NAME")
+    @NotNull
+    @Size(max = 200)
+    private String lastName;
 
 
     @Column(name = "MOBILE_PHONE")
@@ -80,13 +95,98 @@ public class UserEntity implements Serializable {
     @Column(name = "LAST_LOGIN_AT")
     private ZonedDateTime lastLoginAt;
 
-    //Define a private attibute named "salt" of type String, explictly specify its column name as "SALT" with notnull constraint and its maximum size should be 200
+    //Define a private attibute named "salt" of type String,
+    // explictly specify its column name as "SALT"
+    // with notnull constraint and its maximum size should be 200
+    @Column(name = "SALT")
+    @NotNull
+    @Size(max = 200)
+    private String salt;
 
 
-    //After defining all the attributes with mentioned annotations generate getters and setters for all the attributes
+    //After defining all the attributes with mentioned annotations
+    // generate getters and setters for all the attributes
 
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public ZonedDateTime getLastLoginAt() {
+        return lastLoginAt;
+    }
+
+    public void setLastLoginAt(ZonedDateTime lastLoginAt) {
+        this.lastLoginAt = lastLoginAt;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
 
     @Override
     public boolean equals(Object obj) {
